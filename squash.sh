dac33e6e2601ed286f05acf625a7ea2e6cb4c365
GoxySvitac <goran.svitlica@live.com>
2026-08-08 12:38:18 +0200

    squished

--- squash.sh
+++ squash.sh
@@ -1,5 +1,5 @@
 git reset --soft $(git rev-list --max-parents=0 HEAD)
-git commit -m "Squished changes"
+git commit -m "squished"
 git push --force
-sleep 35
+sleep 40
 paplay /home/goran/Desktop/Q/midi/gox/1024.wav
